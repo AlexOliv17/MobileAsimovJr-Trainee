acf249cfd41921219872260acab9f579ba27bf02
Alex Oliveira <alexoalves10@gmail.com>
2024-06-11 19:25:20 -0300

    Modificações no projeto teste realizados (Simulador Par ou Ímpar)

--- Semana 2/teste/lib/main.dart
+++ Semana 2/teste/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -28,10 +29,11 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 1, 4, 196)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Simulador de Par ou Impar'),
     );
   }
 }
@@ -56,6 +58,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String _parImpar = "";
 
   void _incrementCounter() {
     setState(() {
@@ -64,7 +67,16 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      Random random = Random();
+      _counter = random.nextInt(1000);
+
+      if (_counter % 2 == 0) {
+        print("Counter is par");
+        _parImpar = "par";
+      } else {
+        print("Counter is impar");
+        _parImpar = "impar";
+      }
     });
   }
 
@@ -106,10 +118,15 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'O número sorteado foi:',
             ),
             Text(
               '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Text('O número é: '),
+            Text(
+              '$_parImpar',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
